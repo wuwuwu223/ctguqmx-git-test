6434c4b8e51e0abb05acfc61ac97f17c91368e16
yxr2333 <54535282@qq.com>
2021-07-27 09:50:15 +0800

    1

--- 喻鑫锐/web/js/index.js
+++ 喻鑫锐/web/js/index.js
@@ -4,10 +4,9 @@ layui.use('element', function(){
     //一些事件触发
     let myiframe = $("#myiframe");
     element.on('nav(test)', function(data){
-        // console.log(data);
+        console.log(data);
         var choice = data.context.innerText;
         console.log(choice);
-        console.log(typeof(choice));
         switch (choice){
             case " 默认展开":
                 myiframe.attr('src','./pages/dashboard.html')
@@ -18,6 +17,15 @@ layui.use('element', function(){
                 break;
             case " 评论":
                 myiframe.attr('src',"./pages/comments.html")
+                break;
+            case " 时间线":
+                myiframe.attr('src',"./pages/timeLine.html")
+                break;
+            case " 成果展示":
+                myiframe.attr('src',"./pages/show.html")
+                break;
+            case " 获奖情况":
+                myiframe.attr('src',"./pages/reward.html")
                 break;
         }
     });
