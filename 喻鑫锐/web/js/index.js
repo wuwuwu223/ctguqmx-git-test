28522bf76cf81e488fb5bef9430dfda7c9604159
yxr2333 <54535282@qq.com>
2021-07-28 20:38:58 +0800

    1

--- 喻鑫锐/web/js/index.js
+++ 喻鑫锐/web/js/index.js
@@ -1,3 +1,21 @@
+window.onload = function (){
+    let token = localStorage.getItem("token");
+    // console.log(token);
+    if (token === null){
+        window.location.replace("../index.html");
+    }
+    $.ajax({
+        type: "GET",
+        url: "https://www.iloveyxr.top:8081/parse?token=" +  token,
+        dataType: "json",
+        success: res => {
+            if(res.name !== "admin"){
+                window.location.replace("../index.html");
+            }
+        }
+    });
+}
+
 layui.use('element', function(){
     var element = layui.element;
 
@@ -9,23 +27,23 @@ layui.use('element', function(){
         console.log(choice);
         switch (choice){
             case " 默认展开":
-                myiframe.attr('src','./pages/dashboard.html')
+                myiframe.attr('src','./dashboard.html')
                 break;
             case " 学生列表":
                 console.log("yes");
-                myiframe.attr('src',"./pages/studentList.html")
+                myiframe.attr('src',"./studentList.html")
                 break;
             case " 评论":
-                myiframe.attr('src',"./pages/comments.html")
+                myiframe.attr('src',"./comments.html")
                 break;
             case " 时间线":
-                myiframe.attr('src',"./pages/timeLine.html")
+                myiframe.attr('src',"./timeLine.html")
                 break;
             case " 成果展示":
-                myiframe.attr('src',"./pages/show.html")
+                myiframe.attr('src',"./show.html")
                 break;
             case " 获奖情况":
-                myiframe.attr('src',"./pages/reward.html")
+                myiframe.attr('src',"./reward.html")
                 break;
         }
     });
